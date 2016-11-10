99e1afa89ab9c6a49f245d7b03b8106e29dbe411
Susiane <susianerodrigues@gmail.com>
2016-11-10 21:38:16 -0200

    second commit Susi on MainActivity Add Log in Teste method

--- Verdowth/app/src/main/java/br/com/virtualdatabase/verdowth/MainActivity.java
+++ Verdowth/app/src/main/java/br/com/virtualdatabase/verdowth/MainActivity.java
@@ -3,6 +3,7 @@ package br.com.virtualdatabase.verdowth;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 
@@ -41,9 +42,7 @@ public class MainActivity extends Activity {
     }
 
     public void teste(){
-
+        Log.d("TAG", "Hello Git Hub");
     }
-
-
 
 }
