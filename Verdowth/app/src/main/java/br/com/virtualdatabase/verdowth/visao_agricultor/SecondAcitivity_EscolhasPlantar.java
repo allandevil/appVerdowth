992692f639f82d1edc785414b29861a0f0cb9b6c
Marcos Cabanas Esteves <marcoscesteves@gmail.com>
2016-12-06 21:37:37 -0200

    Teste

---
+++ Verdowth/app/src/main/java/br/com/virtualdatabase/verdowth/visao_agricultor/SecondAcitivity_EscolhasPlantar.java
@@ -0,0 +1,20 @@
+package br.com.virtualdatabase.verdowth.visao_agricultor;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import br.com.virtualdatabase.verdowth.R;
+
+/**
+ * Created by marcoscesteves on 06/12/16.
+ */
+public class SecondAcitivity_EscolhasPlantar extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.escolher_plantar);
+    }
+}
--- Verdowth/app/src/main/java/br/com/virtualdatabase/verdowth/visao_agricultor/SomosParceirosAgricultorFragment.java
+++ Verdowth/app/src/main/java/br/com/virtualdatabase/verdowth/visao_agricultor/SomosParceirosAgricultorFragment.java
@@ -1,11 +1,13 @@
 package br.com.virtualdatabase.verdowth.visao_agricultor;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import br.com.virtualdatabase.verdowth.R;
 
@@ -15,14 +17,23 @@ import br.com.virtualdatabase.verdowth.R;
 
 public class SomosParceirosAgricultorFragment extends Fragment {
 
+    Button goVerdowth;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.visao_agricultor__somos_parceiros, container, false);
-
+        View view = inflater.inflate(R.layout.visao_agricultor__somos_verdowth, container, false);
+        goVerdowth = (Button) view.findViewById(R.id.goVerdowthBtn);
         //Todo: Lembrar de citar o site icons8.com
 
+        goVerdowth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), SecondAcitivity_EscolhasPlantar.class);
+                startActivity(intent);
+            }
+        });
         return view;
     }
+
 }
